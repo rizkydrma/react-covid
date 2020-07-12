dfc308b012c2ba8690fab9c5152e1bc8b4ef16ab
unknown <rizky.darmarazak@gmail.com>
2020-07-13 06:44:27 +0700

    Updates

---
+++ precache-manifest.8f3c3380489f5a7008b26ef292cb95ce.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd1b104c5c04dd61140bb2a8be4ed5ec",
+    "url": "/react-covid/index.html"
+  },
+  {
+    "revision": "828ee36f0cd7b0247cce",
+    "url": "/react-covid/static/css/main.f594b057.chunk.css"
+  },
+  {
+    "revision": "feb6f2c3899b2ca74726",
+    "url": "/react-covid/static/js/2.c69963af.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-covid/static/js/2.c69963af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "828ee36f0cd7b0247cce",
+    "url": "/react-covid/static/js/main.41ea1855.chunk.js"
+  },
+  {
+    "revision": "3e211ec65b0b8fef0721",
+    "url": "/react-covid/static/js/runtime-main.e5134e2e.js"
+  },
+  {
+    "revision": "cb5405cada687eded29cd51e7bdc0aa2",
+    "url": "/react-covid/static/media/banner.cb5405ca.png"
+  },
+  {
+    "revision": "3a1df69d1fdcb14f4bb4a86ff7db787b",
+    "url": "/react-covid/static/media/img-1.3a1df69d.png"
+  },
+  {
+    "revision": "9b2de9c7108edd0d9934bc0a3f1e7f85",
+    "url": "/react-covid/static/media/img-2.9b2de9c7.png"
+  },
+  {
+    "revision": "0b9230f552337d6eda1ed46507c88fef",
+    "url": "/react-covid/static/media/img-logo.0b9230f5.svg"
+  },
+  {
+    "revision": "0aba98e1df5615f1808847a93a9a64ba",
+    "url": "/react-covid/static/media/triple-dot.0aba98e1.svg"
+  }
+]);
